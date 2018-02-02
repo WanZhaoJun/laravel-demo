e43dec9825f816ae4a66bd8cdb07cf0b3ae13f7e
wanzj <1158125340@qq.com>
2018-02-02 13:01:38 +0800

    fix bug

--- app/Fan.php
+++ app/Fan.php
@@ -4,5 +4,15 @@ namespace App;
 
 class Fan extends Model
 {
-    //
+    //粉丝用户
+    public function fuser()
+    {
+//        return $this->hasOne()
+    }
+
+    //关注用户
+    public function suser()
+    {
+
+    }
 }
